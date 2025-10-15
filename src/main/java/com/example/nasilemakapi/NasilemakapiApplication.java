b08547ffff9c3b4fd9a98899003b1bdb21d1b755
TINGWEIJING <tingweijingting2000@gmail.com>
2025-10-15 21:36:58 +0800

    first commit

---
+++ src/main/java/com/example/nasilemakapi/NasilemakapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nasilemakapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasilemakapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasilemakapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nasilemakapi/NasilemakapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasilemakapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasilemakapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
